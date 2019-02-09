c1097674f4a3959098b6d3368c90ea4439f75efb
tavyandy97 <tavy.andy97@gmail.com>
2019-02-09 11:08:27 +0530

    Change current day menu according to time. (eg start showing next day breakfast after current day breakfast is over)

--- data/menu.js
+++ data/menu.js
@@ -25,62 +25,153 @@ function getNumberOfWeek() {
   // return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7)-1;
   return Math.floor(pastDaysOfYear/7);
 }
-var getMenu = (meal , chatId) => {
-  if(!(meal === 'Breakfast' || meal === 'Lunch' || meal == 'Dinner')){
-    return 'Please Select a Valid Meal...';
+
+function getTimeStatus(){
+  const today = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"}));
+  const H = today.getHours();
+  if(H < 9){
+    return 0;
+  }else if((H >= 9) && (H < 14)){
+    return 1;  
+  }else if((H >= 14) && (H < 21)){
+    return 2;
+  }else{
+    return 3;
   }
-  logRequest(meal , chatId);
+}
+
+function getNextDayMenu(){
   const today = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"}));
   const noOfWeeks = getNumberOfWeek();
   if(noOfWeeks %2 == 0){
     switch(today.getDay()){
       case 0:
-        return SUNDAY_MENU_EVEN.menu[meal.toLowerCase()];
+        return MONDAY_MENU_ODD.menu;
         break;
       case 1:
-        return MONDAY_MENU_EVEN.menu[meal.toLowerCase()];
+        return TUESDAY_MENU_EVEN.menu;
         break;
       case 2:
-        return TUESDAY_MENU_EVEN.menu[meal.toLowerCase()];
+        return WEDNESDAY_MENU_EVEN.menu;
         break;
       case 3:
-        return WEDNESDAY_MENU_EVEN.menu[meal.toLowerCase()];
+        return THURSDAY_MENU_EVEN.menu;
         break;
       case 4:
-        return THURSDAY_MENU_EVEN.menu[meal.toLowerCase()];
+        return FRIDAY_MENU_EVEN.menu;
         break;
       case 5:
-        return FRIDAY_MENU_EVEN.menu[meal.toLowerCase()];
+        return SATURDAY_MENU_EVEN.menu;
         break;
       case 6:
-        return SATURDAY_MENU_EVEN.menu[meal.toLowerCase()];
+        return SUNDAY_MENU_EVEN.menu;
         break;   
     }
   }else{
     switch(today.getDay()){
       case 0:
-        return SUNDAY_MENU_ODD.menu[meal.toLowerCase()];
+        return MONDAY_MENU_EVEN.menu;
         break;
       case 1:
-        return MONDAY_MENU_ODD.menu[meal.toLowerCase()];
+        return TUESDAY_MENU_ODD.menu;
         break;
       case 2:
-        return TUESDAY_MENU_ODD.menu[meal.toLowerCase()];
+        return WEDNESDAY_MENU_ODD.menu;
         break;
       case 3:
-        return WEDNESDAY_MENU_ODD.menu[meal.toLowerCase()];
+        return THURSDAY_MENU_ODD.menu;
         break;
       case 4:
-        return THURSDAY_MENU_ODD.menu[meal.toLowerCase()];
+        return FRIDAY_MENU_ODD.menu;
         break;
       case 5:
-        return FRIDAY_MENU_ODD.menu[meal.toLowerCase()];
+        return SATURDAY_MENU_ODD.menu;
         break;
       case 6:
-        return SATURDAY_MENU_ODD.menu[meal.toLowerCase()];
+        return SUNDAY_MENU_ODD.menu;
         break;   
     }
   }
+}
+
+function getPresentDayMenu(){
+  const today = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"}));
+  const noOfWeeks = getNumberOfWeek();
+  if(noOfWeeks %2 == 0){
+    switch(today.getDay()){
+      case 0:
+        return SUNDAY_MENU_EVEN.menu;
+        break;
+      case 1:
+        return MONDAY_MENU_EVEN.menu;
+        break;
+      case 2:
+        return TUESDAY_MENU_EVEN.menu;
+        break;
+      case 3:
+        return WEDNESDAY_MENU_EVEN.menu;
+        break;
+      case 4:
+        return THURSDAY_MENU_EVEN.menu;
+        break;
+      case 5:
+        return FRIDAY_MENU_EVEN.menu;
+        break;
+      case 6:
+        return SATURDAY_MENU_EVEN.menu;
+        break;   
+    }
+  }else{
+    switch(today.getDay()){
+      case 0:
+        return SUNDAY_MENU_ODD.menu;
+        break;
+      case 1:
+        return MONDAY_MENU_ODD.menu;
+        break;
+      case 2:
+        return TUESDAY_MENU_ODD.menu;
+        break;
+      case 3:
+        return WEDNESDAY_MENU_ODD.menu;
+        break;
+      case 4:
+        return THURSDAY_MENU_ODD.menu;
+        break;
+      case 5:
+        return FRIDAY_MENU_ODD.menu;
+        break;
+      case 6:
+        return SATURDAY_MENU_ODD.menu;
+        break;   
+    }
+  }
+}
+
+var getMenu = (meal , chatId) => {
+  if(!(meal === 'Breakfast' || meal === 'Lunch' || meal == 'Dinner')){
+    return 'Please Select a Valid Meal...';
+  }
+  logRequest(meal , chatId);
+  var currentMenu = getPresentDayMenu();
+  var nextDayMenu = getNextDayMenu();
+  switch(getTimeStatus()){
+    case 0:
+      break;
+    case 1:
+      currentMenu.breakfast = nextDayMenu.breakfast;
+      break;
+    case 2:
+      currentMenu.breakfast = nextDayMenu.breakfast;
+      currentMenu.lunch = nextDayMenu.lunch;
+      break;
+    case 3:
+      currentMenu.breakfast = nextDayMenu.breakfast;
+      currentMenu.lunch = nextDayMenu.lunch;
+      currentMenu.dinner = nextDayMenu.dinner;
+      break;
+  }
+  return currentMenu[meal.toLowerCase()];
 };
 
 module.exports = {
